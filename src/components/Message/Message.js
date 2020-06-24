d1d5dc7faf2f645fa581e52ad8aa92f2c749cf7c
NatsuDzn <natsuxgraph@gmail.com>
2020-06-24 18:23:55 +0200

    fix: import error

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,6 +1,6 @@
 import MessageBubble from "./MessageBubble/MessageBubble"
 import MessageButton from "./MessageButton/MessageButton"
 import MessageHeader from "./MessageHeader/MessageHeader"
-import MessageInput from "./MessageInput/MessageInput"
+import MessageBar from "./MessageInput/MessageInput"
 
-export { MessageBubble, MessageButton, MessageHeader, MessageInput }
+export { MessageBubble, MessageButton, MessageHeader, MessageBar }
