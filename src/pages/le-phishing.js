fd9e389f0357ff1bc78543dec41f36befa13b618
Wilem Djennane <wilemdjennane@Air-de-Wilem.home>
2020-06-30 17:33:06 +0200

    feat(home): add routes to other pages

---
+++ src/pages/le-phishing.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/Layout/Layout"
+
+const Phishing = () => {
+  return <Layout>Le phishing</Layout>
+}
+
+export default Phishing
---
+++ src/pages/les-traces-numériques.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/Layout/Layout"
+
+const TracesNumeriques = () => {
+  return <Layout>Les traces numériques</Layout>
+}
+
+export default TracesNumeriques
