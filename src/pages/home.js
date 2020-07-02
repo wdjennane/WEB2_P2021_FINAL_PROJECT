e691e06a20e8eb77bf709652696464842007a910
Nathanael Louzoun <natsuxgraph@gmail.com>
2020-07-02 09:04:05 +0200

    feat(notification): add notification  (#16)
    * feat(notif): add react-toastify
    
    * fix: use the website font

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,41 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { Link } from "react-router-dom"
 import Layout from "../components/Layout/Layout"
-import HeaderNotification from "../components/Applications/HeaderNotice/HeaderNotification"
 import { HomeContainer } from "../components/Applications/HomeStyle"
 import Applications from "../components/Applications/Applications"
 
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+
 const Home = () => {
+  useEffect(() => {
+    toast("✉️ EXPLICATION NON LU", {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    })
+  }, [])
+
   return (
     <Layout>
       <HomeContainer>
-        <HeaderNotification />
+        <Link to="/message">
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+        </Link>
         <h1>Les dangers du web</h1>
         <Applications />
       </HomeContainer>
