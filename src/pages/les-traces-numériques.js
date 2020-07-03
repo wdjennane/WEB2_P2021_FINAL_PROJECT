d871c28c14b9b881aa4cd536da1a88345659d000
NatsuDzn <natsuxgraph@gmail.com>
2020-07-03 11:54:35 +0200

    refactor(message): cleaner way to use notification

--- src/pages/les-traces-numériques.js
+++ src/pages/les-traces-numériques.js
@@ -1,40 +1,16 @@
-import React, { useEffect } from "react"
-import { Link } from "react-router-dom"
+import React from "react"
 import Layout from "../components/Layout/Layout"
 import DictionaryCard from "../components/Dictionary/DictionaryCard"
 import { Text, Section } from "../jsStyles/CommonStyle"
-
-import { ToastContainer, toast } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
+import Notification from "../components/Notification/Notification"
 
 const TracesNumeriques = () => {
-  useEffect(() => {
-    toast("✉️ EXPLICATION NON LU", {
-      position: "top-center",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    })
-  }, [])
-
   return (
     <Layout title="Les traces numériques">
-      <Link to="/les-traces-numeriques/message">
-        <ToastContainer
-          position="top-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-      </Link>
+      <Notification
+        text="✉️ EXPLICATION NON LU"
+        path="/les-traces-numeriques/message"
+      />
       <Section>
         <DictionaryCard>
           <Text>
