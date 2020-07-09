9081d1bc4d72cb4fcdf7c4441be8d5cb8336cff7
Jacky Shao <jacky.shaows@gmail.com>
2020-07-09 16:43:11 +0200

    feat(home): add disclaimer (#34)

--- src/jsStyles/pages/HomeStyle.js
+++ src/jsStyles/pages/HomeStyle.js
@@ -2,6 +2,7 @@ import styled from "styled-components"
 import Colors from "../../jsStyles/Colors"
 
 const HomeContainer = styled.div`
+  position: relative;
   height: 100%;
   display: flex;
   flex-direction: column;
@@ -17,4 +18,66 @@ const HomeTitle = styled.h1`
   font-weight: 600;
 `
 
-export { HomeContainer, HomeTitle }
+const HomeDisclaimerButton = styled.p`
+  position: absolute;
+  right: 0.5rem;
+  bottom: 0.5rem;
+  color: white;
+  padding: 0.25rem 0.5rem;
+  border-radius: 4px;
+  font-size: 0.75rem;
+  background-color: ${Colors.Brand};
+`
+
+const HomeDisclaimerCloseButton = styled.img`
+  height: 10px;
+  align-self: flex-end;
+`
+
+const HomeDisclaimerModalWrapper = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const HomeDisclaimerModal = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  padding: 0.25rem 0.5rem;
+  border-radius: 4px;
+  font-size: 0.8rem;
+  box-shadow: 0px 5px 40px rgba(146, 146, 146, 0.25);
+  padding: 1rem;
+  line-height: 1.25rem;
+  z-index: 1;
+`
+
+const HomeDisclaimerModalTitle = styled.h1`
+  color: ${Colors.TextDark};
+  font-weight: bold;
+  font-size: 1.05rem;
+  margin: 0 0 0.5rem 0;
+`
+
+const HomeDisclaimerModalOverlay = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+`
+
+export {
+  HomeContainer,
+  HomeTitle,
+  HomeDisclaimerModalWrapper,
+  HomeDisclaimerModal,
+  HomeDisclaimerModalOverlay,
+  HomeDisclaimerModalTitle,
+  HomeDisclaimerCloseButton,
+  HomeDisclaimerButton,
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../components/Layout/Layout"
 import Application from "../components/Application/Application"
 import Notification from "../components/Notification/Notification"
-import { HomeContainer, HomeTitle } from "../jsStyles/pages/HomeStyle"
+import {
+  HomeContainer,
+  HomeTitle,
+  HomeDisclaimerButton,
+  HomeDisclaimerModal,
+  HomeDisclaimerModalOverlay,
+  HomeDisclaimerCloseButton,
+  HomeDisclaimerModalTitle,
+  HomeDisclaimerModalWrapper,
+} from "../jsStyles/pages/HomeStyle"
 import { ApplicationWrapper } from "../components/Application/ApplicationStyle"
 import WebDangerIcon from "../assets/icons/les-dangers-du-web-iconn.svg"
 import RevengePornIcon from "../assets/icons/le-revenge-porn-icon.svg"
 import PhishingIcon from "../assets/icons/le-phishing-icon.svg"
 import DigitalTraceIcon from "../assets/icons/les-traces-numeriques-icon.svg"
+import CloseIcon from "../assets/icons/close.svg"
 
 const Home = () => {
   const applications = [
@@ -32,6 +42,9 @@ const Home = () => {
       image: RevengePornIcon,
     },
   ]
+
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <Layout>
       <HomeContainer>
@@ -47,6 +60,27 @@ const Home = () => {
             />
           ))}
         </ApplicationWrapper>
+        <HomeDisclaimerButton onClick={() => setIsOpen(true)}>
+          Disclaimer
+        </HomeDisclaimerButton>
+        {isOpen && (
+          <HomeDisclaimerModalWrapper>
+            <HomeDisclaimerModalOverlay onClick={() => setIsOpen(false)} />
+            <HomeDisclaimerModal>
+              <HomeDisclaimerCloseButton
+                src={CloseIcon}
+                alt="close"
+                onClick={() => setIsOpen(false)}
+              />
+              <HomeDisclaimerModalTitle>Disclaimer</HomeDisclaimerModalTitle>
+              Ce site a été réalisé à des fins pédagogiques dans le cadre du
+              cursus Bachelor de l’école HETIC. Les contenus présentés n'ont pas
+              fait l'objet d'une demande de droit d'utilisation. Ce site ne sera
+              en aucun cas exploité à des fins commerciales et ne sera pas
+              publié”
+            </HomeDisclaimerModal>
+          </HomeDisclaimerModalWrapper>
+        )}
       </HomeContainer>
     </Layout>
   )
